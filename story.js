8b32db00d4e2006959e2454e3813bbdb4f805f37
Austin Burdine <acburdine@gmail.com>
2015-10-08 07:12:37 -0500

    fix some more typos

--- story.js
+++ story.js
@@ -29,7 +29,7 @@ var storyData = [
 },
 // 1
 {
-    message: "You are a freshman in high school. Fifteen years of age, you recently moved to Boston after your father recently got a promotion at the city/s sewage plant as the new foreman. Your family has recently bought a middle-class, mid-American home in the city/s suburbs.\\\\On your left, lives the Johnsons and their daughter, Sally. Sally has an enthusiastic, upbeat personality. She loves people and the spotlight.\\\\On your right, the Davidsons and their boy, Ken. Ken has a kind-heart, but lets the fear of judgment hold him back. He is a recluse but loves his mother dearly.",
+    message: "You are a freshman in high school. Fifteen years of age, you recently moved to Boston after your father recently got a promotion at the city/s sewage plant as the new foreman. Your family has recently bought a middle-class, mid-American home in the city\'s suburbs.\\\\On your left, lives the Johnsons and their daughter, Sally. Sally has an enthusiastic, upbeat personality. She loves people and the spotlight.\\\\On your right, the Davidsons and their boy, Ken. Ken has a kind-heart, but lets the fear of judgment hold him back. He is a recluse but loves his mother dearly.",
     proceed: {
         msg: 'Next',
         next: 2
@@ -37,7 +37,7 @@ var storyData = [
 },
 // 2 - fresh neutral
 {
-    message: "It/s early fall on the first day of school. As you leave your house, you notice both Ken and Sally preparing to head to school themselves.  Sally is catching a ride with some of her friends and there is only one seat left in the car.  Ken, on the other hand, has just stepped out of his house and is about to walk to school alone.\\\\You have the opportunity to go to school with either one. Who do you choose?",
+    message: "It\'s early fall on the first day of school. As you leave your house, you notice both Ken and Sally preparing to head to school themselves.  Sally is catching a ride with some of her friends and there is only one seat left in the car.  Ken, on the other hand, has just stepped out of his house and is about to walk to school alone.\\\\You have the opportunity to go to school with either one. Who do you choose?",
     proceed: [
         {
             msg: "Walk to school with Ken",
@@ -63,7 +63,7 @@ var storyData = [
 },
 // 3 - fresh ken
 {
-    message: "One morning, while you and Ken are on your way to school together, Ken opens up to you.  He tells you about how his father left him and his mother when he was only 5 years old.  Since then, he tells you that his mother met a guy and remarried.\\\\However, as Ken continues, you learn that his relationship with his stepdad is questionable at best and that Ken deals with physical and verbal abuse from time to time.\\\\How do you react to what you/ve just heard?",
+    message: "One morning, while you and Ken are on your way to school together, Ken opens up to you.  He tells you about how his father left him and his mother when he was only 5 years old.  Since then, he tells you that his mother met a guy and remarried.\\\\However, as Ken continues, you learn that his relationship with his stepdad is questionable at best and that Ken deals with physical and verbal abuse from time to time.\\\\How do you react to what you\'ve just heard?",
     proceed: [
         {
             msg: "Be supportive and try to console Ken.",
@@ -96,7 +96,7 @@ var storyData = [
 },
 // 4 - fresh sally
 {
-    message: "One day at school, after biology class together, Sally approaches you to see what you got on your last Biology exam. You did well and got a 93% on your exam and curiously, Sally got the exact same score.  You ask her about her score and she admits to you that she copied off your exam.\\\\She checks to see your reaction and asks if you wouldn/t be opposed to her continuing to copy off your future exams.  After all, biology is her weakest subject and she doesn/t want to fail.\\\\What do you choose to do?",
+    message: "One day at school, after biology class together, Sally approaches you to see what you got on your last Biology exam. You did well and got a 93% on your exam and curiously, Sally got the exact same score.  You ask her about her score and she admits to you that she copied off your exam.\\\\She checks to see your reaction and asks if you wouldn\'t be opposed to her continuing to copy off your future exams.  After all, biology is her weakest subject and she doesn\'t want to fail.\\\\What do you choose to do?",
     proceed: [
         {
             msg: "Shrug and inform her that you don\'t mind her copying off your exams in the future.",
@@ -128,7 +128,7 @@ var storyData = [
 },
 // 5 - soph netural
 {
-    message: "A year has passed and you can/t believe that you/re already back in school again.  You/re not sure if you/re ready for your sophomore year, but you/re feeling optimistic.  As more students shamble into your history class, you notice Sally and Ken among them.  They both take their seats around you and class begins.\\\\Surprise! A group project gets assigned on the very first day! The entire class grumbles, but luckily, the three of you get placed in a group together. The teacher explains that each group is supposed to give a ten minute presentation on a certain natural or man-made monument.  Ken offers the idea of doing the project on Mt. Kilimanjaro, but Sally wants to keep it simple and thinks it would be better to do the project on the Eiffel Tower.\\\\What do you choose to do?",
+    message: "A year has passed and you can\'t believe that you\'re already back in school again.  You\'re not sure if you\'re ready for your sophomore year, but you\'re feeling optimistic.  As more students shamble into your history class, you notice Sally and Ken among them.  They both take their seats around you and class begins.\\\\Surprise! A group project gets assigned on the very first day! The entire class grumbles, but luckily, the three of you get placed in a group together. The teacher explains that each group is supposed to give a ten minute presentation on a certain natural or man-made monument.  Ken offers the idea of doing the project on Mt. Kilimanjaro, but Sally wants to keep it simple and thinks it would be better to do the project on the Eiffel Tower.\\\\What do you choose to do?",
     proceed: [
         {
             msg: "Side with Ken and vote on Mt. Kilimanjaro.",
@@ -149,7 +149,7 @@ var storyData = [
                     next = (parseInt(ken) > parseInt(sally)) ? 6 : 7;
                 
                 return {
-                    response: "The two of the stare at you expectantly before resuming their argument with one another.  Both refuse to back down and eventually the teacher intervenes and forces your group to work on Mt. Rushmore instead.",
+                    response: "The two of them stare at you expectantly before resuming their argument with one another.  Both refuse to back down and eventually the teacher intervenes and forces your group to work on Mt. Rushmore instead.",
                     next: next
                 };
             }
@@ -218,7 +218,7 @@ var storyData = [
         {
             msg: "Inform her that you don\'t know him enough to make an accurate judgment of his character.",
             next: {
-                response: "She is appreciative that you were willing to take the time to talk with her. Even though you didn\t offer her much help.",
+                response: "She is appreciative that you were willing to take the time to talk with her. Even though you didn\'t offer her much help.",
                 next: 8
             }
         },
@@ -257,7 +257,7 @@ var storyData = [
                     next = (parseInt(ken) > parseInt(sally)) ? 9 : 10;
                 
                 return {
-                    response: "Your father gives you a puzzled look, before nodding and closing your door.  He ends up selling the ticket like you wanted and used the money to buy himself a Justin Bieber shirt at the concert, because he is a Belieber.",
+                    response: "Your father gives you a puzzled look, before nodding and closing your door.  He ends up selling the ticket like you wanted and using the money to buy himself a Justin Bieber shirt at the concert, because he is a Belieber.",
                     next: next
                 };
             }
